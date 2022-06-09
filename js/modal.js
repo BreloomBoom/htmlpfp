7f2a5bec30294e73990b47bd9c15bf3fe6756958
Ronald Chiang <rschaing@gmail.com>
2022-06-10 07:53:02 +1000

    text size changed

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,14 @@
-var modal = document.getElementById("myModal");
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+let modal = document.getElementById("myModal");
+let img = document.getElementById("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+let span = document.getElementsByClassName("close")[0];
+
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
-
-var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
   modal.style.display = "none";
